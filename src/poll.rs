dfda08e4d35c22ca75a2c24f1fe2397ed8b02c05
Alex Stuart <alex@hod.lan>
2023-11-25 12:38:00 -0800

    Fetch all pages of playlist items

--- src/poll.rs
+++ src/poll.rs
@@ -18,23 +18,30 @@ fn main() {
     let playlist_id_string = "6XB4xIqNs20vEGubRKvnAv";
     let polling_interval = 60;
 
-    let playlist_id = PlaylistId::from_id(playlist_id_string).expect("Invalid ID for playlist");
-    let mut prev_playlist = spotify.playlist(playlist_id.clone(), None, None).expect("Cannot get initial playlist");
+    let playlist_id = PlaylistId::from_id(playlist_id_string)
+        .expect("Invalid ID for playlist");
+    let mut prev_playlist = spotify.playlist(playlist_id.clone(), None, None)
+        .expect("Cannot get initial playlist");
+    let mut prev_items = get_tracks(&spotify, &playlist_id);
+
     thread::sleep(Duration::from_secs(polling_interval));
     loop {
+        println!("{} tracks", prev_items.len());
         if let Ok(curr_playlist) = spotify.playlist(playlist_id.clone(), None, None) {
-            if let Some(spans) = compare_playlists(&curr_playlist, &prev_playlist) {
+            if curr_playlist.snapshot_id != prev_playlist.snapshot_id {
+                let curr_items = get_tracks(&spotify, &playlist_id);
+                let spans = compare_playlists(&curr_items, &prev_items);
                 for s in spans {
                     match s.tag {
                         Insert => {
-                            for (index, (maybe_user, maybe_track)) in &get_tracks(&curr_playlist).iter().enumerate().collect::<Vec<_>>()[s.b_start .. s.b_end] {
-                                if let Some(t) = maybe_track {
+                            for (index, item) in &curr_items.iter().enumerate().collect::<Vec<_>>()[s.b_start .. s.b_end] {
+                                if let Some(t) = item.track.as_ref().and_then(from_track) {
                                     for address in recipient_addresses {
                                         email_notification(
                                             address,
                                             match &t.preview_url {
-                                                Some(link) => format!("{} added a track at position {}:\n{}", user_name(maybe_user), index + 1, link),
-                                                None => format!("{} added {} at position {}.", user_name(maybe_user), artist_track_name(t), index + 1),
+                                                Some(link) => format!("{} added a track at position {}:\n{}", user_name(&item.added_by), index + 1, link),
+                                                None => format!("{} added {} at position {}.", user_name(&item.added_by), artist_track_name(&t), index + 1),
                                             }
                                         )
                                     }
@@ -42,14 +49,14 @@ fn main() {
                             }
                         }
                         Delete => {
-                            for (index, (maybe_user, maybe_track)) in &get_tracks(&prev_playlist).iter().enumerate().collect::<Vec<_>>()[s.a_start .. s.a_end] {
-                                if let Some(t) = maybe_track {
+                            for (index, item) in &prev_items.iter().enumerate().collect::<Vec<_>>()[s.a_start .. s.a_end] {
+                                if let Some(t) = item.track.as_ref().and_then(from_track) {
                                     for address in recipient_addresses {
                                         email_notification(
                                             address,
                                             match &t.preview_url {
-                                                Some(link) => format!("Someone removed a track from position {}, originally added by {}:\n{}", index + 1, user_name(maybe_user), link),
-                                                None => format!("Someone removed {} from position {}, originally added by {}.", artist_track_name(t), index + 1, user_name(maybe_user)),
+                                                Some(link) => format!("Someone removed a track from position {}, originally added by {}:\n{}", index + 1, user_name(&item.added_by), link),
+                                                None => format!("Someone removed {} from position {}, originally added by {}.", artist_track_name(&t), index + 1, user_name(&item.added_by)),
                                             }
                                         )
                                     }
@@ -57,31 +64,31 @@ fn main() {
                             }
                         }
                         Replace => {
-                            let curr_block = &get_tracks(&curr_playlist)[s.b_start .. s.b_end];
-                            let prev_block = &get_tracks(&prev_playlist)[s.a_start .. s.a_end];
+                            let curr_block = &curr_items.iter().enumerate().collect::<Vec<_>>()[s.b_start .. s.b_end];
+                            let prev_block = &prev_items.iter().enumerate().collect::<Vec<_>>()[s.a_start .. s.a_end];
                             assert!(curr_block.len() == prev_block.len());
-                            let zipped = curr_block.iter().zip(prev_block.iter()).enumerate().collect::<Vec<_>>();
-                            for (index, (new, old)) in zipped {
+                            let zipped = curr_block.iter().zip(prev_block.iter()).collect::<Vec<_>>();
+                            for ((index, new), (_, old)) in zipped {
                                 for address in recipient_addresses {
-                                    if let (new_user, Some(new_track)) = new {
+                                    if let Some(new_track) = new.track.as_ref().and_then(from_track) {
                                         email_notification(
                                             address,
-                                            if let (old_user, Some(old_track)) = old {
+                                            if let Some(old_track) = old.track.as_ref().and_then(from_track) {
                                                 match (&new_track.preview_url, &old_track.preview_url) {
-                                                    (Some(new_link), Some(old_link)) => format!("{} replaced a track, originally added by {}, at position {}:\n\nNew track:\n{}\n\nOld track:\n{}", user_name(new_user), user_name(old_user), index + 1, new_link, old_link),
-                                                    _ => format!("{} replaced a track, originally added by {}, at position {}:\n\nNew track:\n{}\n\nOld track:\n{}", user_name(new_user), user_name(old_user), index + 1, artist_track_name(new_track), artist_track_name(old_track)),
+                                                    (Some(new_link), Some(old_link)) => format!("{} replaced a track, originally added by {}, at position {}:\n\nNew track:\n{}\n\nOld track:\n{}", user_name(&new.added_by), user_name(&old.added_by), index + 1, new_link, old_link),
+                                                    _ => format!("{} replaced a track, originally added by {}, at position {}:\n\nNew track:\n{}\n\nOld track:\n{}", user_name(&new.added_by), user_name(&old.added_by), index + 1, artist_track_name(&new_track), artist_track_name(&old_track)),
                                                 }
                                             }
                                             else {
-                                                format!("{} added {} at position {}", user_name(new_user), new_track.name, index + 1)
+                                                format!("{} added {} at position {}", user_name(&new.added_by), new_track.name, index + 1)
                                             }
                                         );
                                     }
                                     else {
-                                        if let (old_user, Some(old_track)) = old {
+                                        if let Some(old_track) = old.track.as_ref().and_then(from_track) {
                                             email_notification(
                                                 address,
-                                                format!("Someone removed {} at position {}, originally added by {}", old_track.name, index + 1, user_name(old_user))
+                                                format!("Someone removed {} at position {}, originally added by {}", old_track.name, index + 1, user_name(&old.added_by))
                                             );
                                         }
                                     }
@@ -92,24 +99,18 @@ fn main() {
                     }
                 }
                 prev_playlist = curr_playlist;
+                prev_items = curr_items;
             }
         }
         thread::sleep(Duration::from_secs(polling_interval));
     }
 }
 
-fn compare_playlists(curr_playlist: &FullPlaylist, prev_playlist: &FullPlaylist) -> Option<Vec<Span>> {
-    if curr_playlist.snapshot_id != prev_playlist.snapshot_id {
-        let curr_tracks: Vec<Option<TrackId<'_>>> = curr_playlist.tracks.items.iter().map(|t| t.track.as_ref().and_then(from_track).map(|t| t.id.clone().unwrap())).collect();
-        let prev_tracks: Vec<Option<TrackId<'_>>> = prev_playlist.tracks.items.iter().map(|t| t.track.as_ref().and_then(from_track).map(|t| t.id.clone().unwrap())).collect();
-        let differ = Differ::new(&prev_tracks, &curr_tracks);
-        return Some(differ.spans());
-    }
-    return None;
-}
-
-fn get_tracks(playlist: &FullPlaylist) -> Vec<(Option<&PublicUser>, Option<&FullTrack>)> {
-    return playlist.tracks.items.iter().map(|t| (t.added_by.as_ref(), t.track.as_ref().and_then(from_track))).collect();
+fn compare_playlists(curr_playlist: &Vec<PlaylistItem>, prev_playlist: &Vec<PlaylistItem>) -> Vec<Span> {
+    let curr_tracks: Vec<Option<TrackId<'_>>> = curr_playlist.iter().map(|t| t.track.as_ref().and_then(from_track).map(|t| t.id.clone().unwrap())).collect();
+    let prev_tracks: Vec<Option<TrackId<'_>>> = prev_playlist.iter().map(|t| t.track.as_ref().and_then(from_track).map(|t| t.id.clone().unwrap())).collect();
+    let differ = Differ::new(&prev_tracks, &curr_tracks);
+    return differ.spans();
 }
 
 fn from_track(item: &PlayableItem) -> Option<&FullTrack> {
@@ -119,7 +120,22 @@ fn from_track(item: &PlayableItem) -> Option<&FullTrack> {
     }
 }
 
-fn user_name(maybe_user: &Option<&PublicUser>) -> String {
+fn get_tracks(spotify: &ClientCredsSpotify, playlist_id: &PlaylistId) -> Vec<PlaylistItem> {
+    let mut items = Vec::new();
+    let mut offset = 0;
+    loop {
+        let page = spotify.playlist_items_manual(playlist_id.to_owned(), None, None, None, Some(offset)).expect("Error fetching playlist items");
+        offset += page.items.len() as u32;
+        items.extend(page.items);
+
+        if page.next.is_none() {
+            break;
+        }
+    }
+    return items;
+}
+
+fn user_name(maybe_user: &Option<PublicUser>) -> String {
     match maybe_user {
         Some(user) => user.id.to_string(),
         None => String::from("Someone")
